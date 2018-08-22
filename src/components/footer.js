49176a6ea9ff7b8ad7f56059d6fe78b267b64104
Panos K <lkullolli@gmail.com>
2018-08-22 09:06:39 +0400

    Additional fixes

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Footer = (props) => {
     const allNotes = props.notes.length
-    let completedNotes = props.notes.filter(n => n.completed).length
-    let incompletedNotes = props.notes.filter(n => !n.completed).length
+    const completedNotes = props.notes.filter(n => n.completed).length
+    const incompletedNotes = props.notes.filter(n => !n.completed).length
     return (
         <div className="footer">
-            <span className="filter all-notes" onClick={props.allNotes}>All {allNotes}</span> &nbsp;
-            <span className="filter completed" onClick={props.completedNotes}>Completed {completedNotes}</span> &nbsp;
-            <span className="filter incompleted" onClick={props.incompletedNotes}>Incompleted {incompletedNotes}</span>
+            <button className="btn btn-filter" onClick={props.allNotes}>All {allNotes}</button> &nbsp;
+            <button className="btn btn-filter" onClick={props.completedNotes}>Completed {completedNotes}</button> &nbsp;
+            <button className="btn btn-filter" onClick={props.incompletedNotes}>Incompleted {incompletedNotes}</button>
         </div>
     );
 }
